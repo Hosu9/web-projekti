738b3908c6b3b4d911c0bb0cd53f44ef1b395fad
Rokolampi <144310397+Rokolampi@users.noreply.github.com>
2025-04-03 16:31:01 +0300

    BowlingRules component created

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ import React from "react";
 import { ScoreProvider } from "./context/Scorecontext.js";
 import PlayerForm from "./components/PlayerForm.js";
 import Scoreboard from "./components/Scoreboard";
+import BowlingRules from "./components/BowlingRules.js";
 
 const App = () => {
   return (
     <ScoreProvider>
       <div className="container">
+        <BowlingRules/>
         <h1>Bowling Score Keeper</h1>
         <PlayerForm />
         <Scoreboard />
