1a34fd3e2d69cb0f76f7c063709045518c6581b8
KaritaLaine <44746@edu.raseko.fi>
2025-04-06 18:44:48 +0300

    style: wrap pages with the layout component

--- src/pages/front-page.jsx
+++ src/pages/front-page.jsx
@@ -1,10 +1,18 @@
+import React from "react";
+import { Layout } from "../components/layout";
+
 export const FrontPage = () => {
   return (
-    <div className="frontpage">
-      <h1>Frontpage - under construction </h1>
-      <a href="/score-keeper">Scorekeeper</a>
-      <br />
-      <a href="/rules">Rules - needs a path </a>
-    </div>
+    <Layout>
+      <div className="frontpage">
+        <div className="fp-content">
+          <h1 className="home-header">Home </h1>
+          <p>
+            Front page is under construction! <br />
+            Also, rules need routing to work <br />
+          </p>
+        </div>
+      </div>
+    </Layout>
   );
 };
